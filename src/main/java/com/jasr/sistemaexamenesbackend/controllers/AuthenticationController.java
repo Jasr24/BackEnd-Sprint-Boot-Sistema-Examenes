b810453ec07fe92bd27360656c5d74f471dae4e7
Andres <josex.1995@hotmail.com>
2022-10-05 18:04:09 -0500

    Agregando metodo obtenerUsuarioActual() en la clase AuthentucationController

--- src/main/java/com/jasr/sistemaexamenesbackend/controllers/AuthenticationController.java
+++ src/main/java/com/jasr/sistemaexamenesbackend/controllers/AuthenticationController.java
@@ -1,5 +1,7 @@
 package com.jasr.sistemaexamenesbackend.controllers;
 
+import java.security.Principal;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -8,6 +10,7 @@ import org.springframework.security.authentication.DisabledException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,6 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.jasr.sistemaexamenesbackend.Jwt.JwtRequest;
 import com.jasr.sistemaexamenesbackend.Jwt.JwtResponse;
 import com.jasr.sistemaexamenesbackend.configuraciones.JwtUtil;
+import com.jasr.sistemaexamenesbackend.entidades.Usuario;
 import com.jasr.sistemaexamenesbackend.servicios.Interfases.UserDetailsService;
 
 @RestController
@@ -50,5 +54,11 @@ public class AuthenticationController {
         }catch (BadCredentialsException  badCredentialsException){
             throw new Exception("CREDENCIALES INVALIDAS " + badCredentialsException.getMessage());
         }
+    }
+
+    //Devuelve el usuario que esta en seccion. el actual usuario.
+    @GetMapping("/actual-usuario")
+    public Usuario obtenerUsuarioActual(Principal principal){
+        return (Usuario) this.userDetailsService.loadUserByUsername(principal.getName());
     }
 }
